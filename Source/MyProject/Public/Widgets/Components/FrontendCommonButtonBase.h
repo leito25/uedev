ca73d6857c2c53acbf5e263e52fb5fb10264ec7f
Leonardo Quinones <leito25@gmail.com>
2026-07-29 18:49:04 -0500

    On press any kay funtionality implemented

---
+++ Source/MyProject/Public/Widgets/Components/FrontendCommonButtonBase.h
@@ -0,0 +1,41 @@
+// Leonardo Quinones - All Right Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+#include "FrontendCommonButtonBase.generated.h"
+
+class UCommonTextBlock;
+
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta = (DisableNaiveTick))
+class MYPROJECT_API UFrontendCommonButtonBase : public UCommonButtonBase
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetButtonText(FText InText);
+
+private:
+	//~ Begin UUserWidget Interface
+	virtual void NativePreConstruct() override;
+	//~ End UUserWidget Interface
+	
+	//**** Bound Widgets ****//
+	UPROPERTY(meta = (BindWidgetOptional))
+	UCommonTextBlock* CommonTextBlock_ButtonText;
+	//**** Bound Widgets ****//
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Frontend Button", meta = (AllowPrivateAccess = "true"))
+	FText ButtonDisplayText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Frontend Button", meta = (AllowPrivateAccess = "true"))
+	bool bUserUpperCaseForButtonText = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Frontend Button", meta = (AllowPrivateAccess = "true"))
+	FText ButtonDescriptionText;
+};
